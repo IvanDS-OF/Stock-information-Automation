d5eec5b2fdbb87f21412fc0f382f67c6e18aa9d4
IvanDS <ivan.durans@outlook.com>
2024-07-31 23:25:38 -0600

    Adding the file to create the project

---
+++ project.py
@@ -0,0 +1,34 @@
+##This code is to join all the infroamtion and the previous programs
+#THat now enable me to create the final project
+
+#Attampt 1
+
+#Libraries needed
+
+import requests
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service 
+from selenium.webdriver.common.by import By
+from time import sleep
+
+from bs4 import BeautifulSoup
+from time import sleep
+
+
+#Now we will create the functions that will define the 
+#structure of the project
+
+
+#Define some globlan and important variables
+
+
+
+	#Open the broser in the correct page
+
+
+
+
+
+
+
